19a9d09e59c2fafe503cd37dadcdc36becc5f230
surya <suryamaddi99@gmail.com>
2024-07-10 10:51:31 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,40 @@
+import { Outlet } from "react-router-dom";
+import ProfileFunctionalComponet from "./Profile";
+import Profile from "./ProfileClass";
+import { Component } from "react";
+import UserContext from "../utils/UserContext";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    //console.log("Parent - constructor");
+  }
+  componentDidMount() {
+    // Best place to make an Api call
+    //console.log("Parent - componentDidMount");
+  }
+  render() {
+    //console.log("Parent - render");
+    return (
+      <div>
+        <h1>About Us Page</h1>
+
+        <UserContext.Consumer>
+          {({ user }) => (
+            <h4 className="font-bold text-xl p-10">
+              {user.name}- {user.email}
+            </h4>
+          )}
+        </UserContext.Consumer>
+
+        <p>
+          This is the Namaste React Live Course Chapter 07 - Finding the Path 🚀
+        </p>
+        <Profile />
+      </div>
+    );
+  }
+}
+
+export default About;
